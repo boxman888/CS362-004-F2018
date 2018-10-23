7133d29ecd98ad11f9bf479a9fbf43712aacb16e
Zach Morrissey <zachmor05@gmail.com>
2018-10-23 08:21:46 -0700

    update gitignore and finish unittest1

--- projects/morrissz/dominion/unittest1.c
+++ projects/morrissz/dominion/unittest1.c
@@ -1,7 +1,7 @@
 /*
 unit test 1
 
-tests the updateCoins function
+tests the isGameOver() function
 */
 
 #include "dominion.h"
@@ -13,15 +13,28 @@ tests the updateCoins function
 
 int main(int argc, char *argv[]) {
 
-  int player = 0;
-  int bonus = 1;
+  int i;
   int returnValue;
 
   // clear gamestate and initialize game
   struct gameState gs;
   memset(&gs, 0, sizeof(struct gameState));
 
-  returnValue = updateCoins(player, &gs, bonus);
+  // set the default values w/ empty gamestate
+  returnValue = isGameOver(&gs);
+  assert(returnValue == 1);
+
+  // set the supply count to 1 for the first five
+  for (i = 0; i < 25; i++) {
+    gs.supplyCount[i] = 1;
+  }
+  returnValue = isGameOver(&gs);
+  assert(returnValue == 0);
+
+  // set the province count > 0
+  gs.supplyCount[province] = 10;
+  returnValue = isGameOver(&gs);
+  assert(returnValue == 1);
 
   return 0;
 }
